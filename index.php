b3c9e8e870f3afcaaeab3fabf012aba2f6b84eec
Anllelith <anllelith.hg@gmail.com>
2015-06-03 21:39:57 -0500

    edusoft

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang0"en">
+<head>
+<meta charset="UTF-8">
+	<?php include 'inc/head.php'; ?>
+	<title>document</title>
+
+
+</head>
+<body>
+<h1>Hola</h1>
+<?php include 'inc/footer.php'; ?>
+</body>
+</html>
